1a91b341ab57fd60f4630ef4dfd194e89856dc0f
Mike D <mike.dehart@protonmail.com>
2023-10-26 21:23:13 -0400

    Move from old laptop

--- main.tf
+++ main.tf
@@ -14,17 +14,6 @@ data "aws_ami" "app_ami" {
   owners = [var.ami_filter.owner]
 }
 
-# resource "aws_instance" "blog" {
-#   ami           = data.aws_ami.app_ami.id
-#   instance_type = var.instance_type
-
-#   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-#   tags = {
-#     Name = "Learning Terraform"
-#   }
-# }
-
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
 
